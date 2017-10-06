9fb5cacf3b90d6095d38e088b1afc0cc669be925
johnstudarus <john@jhlconsulting.com>
2017-10-06 10:10:18 -0700

    Update ControllerKeystone.sh

--- ControllerKeystone.sh
+++ ControllerKeystone.sh
@@ -1,8 +1,39 @@
 # Controller Only Below
 
+# sanity check - make sure we can reach the controller
+ping controller -c 5 -q
+if [ $? -ne 0 ] ; then
+  echo "controller is unreachable"
+  echo "check /etc/hosts and networking and then restart this script"
+  exit -1
+fi
 
 # private IP addr (10...)
 MY_IP=`hostname -I | xargs -n1 | grep "^10\." | head -1`
+
+
+# general system updates
+apt-get -y update
+
+# non-interactively set a timezone so we're not interactively prompted
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y tzdata
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# OpenStack needs precise time services
+apt-get -y install chrony
+service chrony restart
+
+apt -y install software-properties-common
+add-apt-repository -y cloud-archive:pike
+apt -y update
+apt -y install python-openstackclient
+
+
+# easy modification of .ini configuration files
+apt-get -y install crudini
+
 
 ## rabbitmq
 apt -y install rabbitmq-server
