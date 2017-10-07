f8d0f4172f849ca051f1fdd6c29f011b9d818633
johnstudarus <john@jhlconsulting.com>
2017-10-07 08:51:00 -0700

    Update ComputeNova.sh
    moved into common setup file

--- ComputeNova.sh
+++ ComputeNova.sh
@@ -1,40 +1,5 @@
 # Compute node only below
 
-# sanity check - make sure we can reach the controller
-ping controller -c 5 -q
-if [ $? -ne 0 ] ; then
-  echo "controller is unreachable"
-  echo "check /etc/hosts and networking and then restart this script"
-  read -p "press a key"
-  exit -1
-fi
-
-# private IP addr (10...)
-MY_IP=`hostname -I | xargs -n1 | grep "^10\." | head -1`
-
-# general system updates
-apt-get -y update
-
-# non-interactively set a timezone so we're not interactively prompted
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y tzdata
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-# OpenStack needs precise time services
-apt-get -y install chrony
-service chrony restart
-
-# general OpenStack packages
-apt-get -y install software-properties-common
-add-apt-repository -y cloud-archive:pike
-apt-get -y update
-apt-get -y install python-openstackclient
-
-
-# easy modification of .ini configuration files
-apt-get -y install crudini
-
 # nova
 apt-get -y install nova-compute
 
