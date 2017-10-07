784e1e05ec78dc238185676c37ba0039bb646c9c
johnstudarus <john@jhlconsulting.com>
2017-10-07 19:19:30 +0000

    create openrc files on all machines for ease of access

--- CommonServerSetup.sh
+++ CommonServerSetup.sh
@@ -36,3 +36,24 @@ apt-get -y install python-openstackclient
 apt-get -y install crudini
 
 
+  
+cat >> admin-openrc << EOF
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN_PASS
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+
+cat >> demo-openrc << EOF
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=DEMO_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
