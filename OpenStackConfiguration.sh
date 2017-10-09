d7aa0556c6ddcfe756e7c02f061dc3670d84b354
johnstudarus <john@jhlconsulting.com>
2017-10-08 20:16:02 -0400

    renaming from AMD64 to x86 added cirros x86

--- OpenStackConfiguration.sh
+++ OpenStackConfiguration.sh
@@ -21,8 +21,15 @@ rm xenial-server-cloudimg-arm64.img
 rm ubuntu-16.04-server-cloudimg-arm64.tar.gz
 
 IMG_URL=https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
-IMG_NAME=CentOS-7-amd64
+IMG_NAME=CentOS-7-x86_64
 OS_DISTRO=centos
+wget -q -O - $IMG_URL | \
+glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
+
+IMG_URL=http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
+IMG_NAME=cirros-x86_64
+OS_DISTRO=cirros
 wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
         --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
