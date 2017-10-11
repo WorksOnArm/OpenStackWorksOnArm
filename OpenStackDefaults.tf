2c69ae3df106299810b89a88075c8406775a3ecb
johnstudarus <john@jhlconsulting.com>
2017-10-11 00:04:18 +0000

    added default images and flavors

---
+++ OpenStackDefaults.tf
@@ -0,0 +1,46 @@
+
+#
+# load up the OpenStack cloud with some default settings and images
+#
+
+resource "null_resource" "openstack-images" {
+
+  depends_on = ["null_resource.controller-openstack"]
+
+  connection {
+    host = "${packet_device.controller.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "DefaultOpenStackImages.sh"
+    destination = "DefaultOpenStackImages.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash DefaultOpenStackImages.sh > DefaultOpenStackImages.out",
+    ]
+  }
+}
+
+resource "null_resource" "openstack-flavors" {
+
+  depends_on = ["null_resource.controller-openstack"]
+
+  connection {
+    host = "${packet_device.controller.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "DefaultOpenStackFlavors.sh"
+    destination = "DefaultOpenStackFlavors.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash DefaultOpenStackFlavors.sh > DefaultOpenStackFlavors.out",
+    ]
+  }
+}
