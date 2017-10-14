eefd59b278830e2bd380c4e2fd5d15763f4cf06a
johnstudarus <john@jhlconsulting.com>
2017-10-14 21:05:55 +0000

    split out image downloads to run in parallel

--- OpenStackDefaults.tf
+++ OpenStackDefaults.tf
@@ -3,7 +3,7 @@
 # load up the OpenStack cloud with some default settings and images
 #
 
-resource "null_resource" "openstack-images" {
+resource "null_resource" "openstack-image-CentOS-7-x86" {
 
   depends_on = ["null_resource.controller-openstack"]
 
@@ -13,13 +13,76 @@ resource "null_resource" "openstack-images" {
   }
 
   provisioner "file" {
-    source      = "DefaultOpenStackImages.sh"
-    destination = "DefaultOpenStackImages.sh"
+    source      = "CentOS-7-x86-Image.sh"
+    destination = "CentOS-7-x86-Image.sh"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "bash DefaultOpenStackImages.sh > DefaultOpenStackImages.out",
+      "bash CentOS-7-x86-Image.sh > CentOS-7-x86-Image.out",
+    ]
+  }
+}
+
+resource "null_resource" "openstack-image-Fedora-26-ARM" {
+
+  depends_on = ["null_resource.controller-openstack"]
+
+  connection {
+    host = "${packet_device.controller.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "Fedora-26-ARM-Image.sh"
+    destination = "Fedora-26-ARM-Image.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash Fedora-26-ARM-Image.sh > Fedora-26-ARM-Image.out",
+    ]
+  }
+}
+
+resource "null_resource" "openstack-image-Cirros-x86" {
+
+  depends_on = ["null_resource.controller-openstack"]
+
+  connection {
+    host = "${packet_device.controller.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "Cirros-x86-Image.sh"
+    destination = "Cirros-x86-Image.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash Cirros-x86-Image.sh > Cirros-x86-Image.out",
+    ]
+  }
+}
+
+resource "null_resource" "openstack-image-Xenial-16_04-ARM-Image" {
+
+  depends_on = ["null_resource.controller-openstack"]
+
+  connection {
+    host = "${packet_device.controller.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "Xenial-16_04-ARM-Image.sh"
+    destination = "Xenial-16_04-ARM-Image.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash Xenial-16_04-ARM-Image.sh > Xenial-16_04-ARM-Image.out",
     ]
   }
 }
