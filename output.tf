219ec0765d9889ba8467954b6ee454da2db483ab
johnstudarus <john@jhlconsulting.com>
2017-10-14 19:39:24 +0000

    display details about nodes (IPs & Type) in Terraform output

--- output.tf
+++ output.tf
@@ -1,3 +1,23 @@
+output "Controller Type" {
+  value = "${packet_device.controller.plan}"
+}
+
+output "Compute x86 Type" {
+  value = "${packet_device.compute-x86.plan}"
+}
+
+output "Compute ARM Type" {
+  value = "${packet_device.compute-arm.plan}"
+}
+
+output "Compute x86 IPs" {
+  value = "${packet_device.compute-x86.*.access_public_ipv4}"
+}
+
+output "Compute ARM IPs" {
+  value = "${packet_device.compute-arm.*.access_public_ipv4}"
+}
+
 output "Horizon dashboard available at" {
   value = "http://${packet_device.dashboard.access_public_ipv4}/horizon/ admin/ADMIN_PASS"
 }
