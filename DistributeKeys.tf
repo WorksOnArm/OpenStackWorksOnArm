72f33e011a8382fbf0463ff3026c2b5766ef3e7c
johnstudarus <john@jhlconsulting.com>
2017-10-17 17:14:53 +0000

    keep a copy of the keys on the dashboard as a backup

--- DistributeKeys.tf
+++ DistributeKeys.tf
@@ -21,3 +21,24 @@ resource "null_resource" "controller-distribute-keys" {
   }
 
 }
+
+# keep a set on the dashboard as a backup in case the controller is down
+
+resource "null_resource" "dashboard-distribute-keys" {
+
+  connection {
+    host = "${packet_device.dashboard.access_public_ipv4}"
+    private_key = "${file("${var.cloud_ssh_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "${file("${var.cloud_ssh_key_path}")}"
+    destination = "openstack_rsa"
+  }
+
+  provisioner "file" {
+    source      = "${file("${var.cloud_ssh_public_key_path}")}"
+    destination = "openstack_rsa.pub"
+  }
+
+}
