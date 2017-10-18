6834e1468630f30f6c57c98846bb5ec2f51a6742
johnstudarus <john@jhlconsulting.com>
2017-10-18 11:16:35 +0000

    displays DNS info

--- output.tf
+++ output.tf
@@ -22,10 +22,18 @@ output "Compute ARM IPs" {
   value = "${packet_device.compute-arm.*.access_public_ipv4}"
 }
 
-output "Horizon dashboard available at" {
+output "Horizon dashboard via IP" {
   value = "http://${packet_device.dashboard.access_public_ipv4}/horizon/ admin/ADMIN_PASS"
 }
 
 output "Controller SSH" {
   value = "ssh root@${packet_device.controller.access_public_ipv4} -i ${var.cloud_ssh_key_path}"
+}
+
+output "Horizon dashboard via DNS" {
+  value = "http://${dnsimple_record.dashboard-dns.hostname}/horizon/ admin/ADMIN_PASS"
+}
+
+output "Controller SSH via DNS" {
+  value = "ssh root@${dnsimple_record.controller-dns.hostname} -i ${var.cloud_ssh_key_path}"
 }
